e4c97204d4ba5d82600a43e6bdb528e53c929e1a
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-03-28 15:34:29 +0300

    stage_1

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,8 +1,13 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
     }
 }
