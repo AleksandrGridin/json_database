bd100c5164fe547313fd744e7cdd40a1bde8dac5
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-03-29 20:41:43 +0300

    stage_2

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,13 +1,35 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String address = "127.0.0.1";
+    private static final int port = 23456;
 
+    public static void main(String[] args) {
+        System.out.println("Client started!");
+
+            try (Socket socket = new Socket(InetAddress.getByName(address), port);
+                 DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+                 DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream())
+            ) {
+
+                String msg = dataIn.readUTF();
+                System.out.println("Sent: Give me a record # " + msg);
+                dataOut.writeUTF(msg);
+                System.out.println("Received: A record # " + msg + " was sent!");
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-}
+
+
--- src/server/Main.java
+++ src/server/Main.java
@@ -1,60 +1,89 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     private static String[] dataBase = new String[100];
+    private static final String ADDRESS = "127.0.0.1";
+    private static final int PORT = 23456;
+
     static {
         initDataBase();
     }
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Server started!");
 
-        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
+        try (ServerSocket serverSocket = new ServerSocket(PORT, 50,
+                InetAddress.getByName(ADDRESS))) {
 
-        while (!(line = scanner.readLine()).equals("exit")) {
 
-            String[] mass = line.split(" ");
+                    try (Socket socket = serverSocket.accept();
+                         DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+                         DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+                         Scanner sc = new Scanner(System.in);) {
 
-            if (mass[0].equals("set")) {
-                try {
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 2; i < mass.length; i++) {
-                        sb.append(mass[i] + " ");
+
+                        int n = 12;
+                        System.out.print("Received: Give me a record # " + n + "\n");
+                        dataOut.writeUTF(String.valueOf(n));
+                        String number = dataIn.readUTF();
+                        System.out.println("Sent: A record # " + number + " was sent!");
+
                     }
-                    dataBase[Integer.parseInt(mass[1]) - 1] = sb.toString().trim();
-                    System.out.println("OK");
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("ERROR");
+
                 }
-            } else if (mass[0].equals("get")) {
-                try {
-                String get = dataBase[Integer.parseInt(mass[1]) - 1];
-                if (get.equals("")) {
-                    System.out.println("ERROR");
-                } else {
-                    System.out.println(get);
-                }
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("ERROR");
-                }
-            } else if (mass[0].equals("delete")) {
-                try {
-                    dataBase[Integer.parseInt(mass[1]) - 1] = "";
-                    System.out.println("OK");
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("ERROR");
-                }
-            } else {
-                break;
-            }
         }
-    }
+
+
+
+
+//        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+//        String line = null;
+//
+//        while (!(line = scanner.readLine()).equals("exit")) {
+//
+//            String[] mass = line.split(" ");
+//
+//            if (mass[0].equals("set")) {
+//                try {
+//                    StringBuilder sb = new StringBuilder();
+//                    for (int i = 2; i < mass.length; i++) {
+//                        sb.append(mass[i] + " ");
+//                    }
+//                    dataBase[Integer.parseInt(mass[1]) - 1] = sb.toString().trim();
+//                    System.out.println("OK");
+//                } catch (IndexOutOfBoundsException e) {
+//                    System.out.println("ERROR");
+//                }
+//            } else if (mass[0].equals("get")) {
+//                try {
+//                String get = dataBase[Integer.parseInt(mass[1]) - 1];
+//                if (get.equals("")) {
+//                    System.out.println("ERROR");
+//                } else {
+//                    System.out.println(get);
+//                }
+//                } catch (IndexOutOfBoundsException e) {
+//                    System.out.println("ERROR");
+//                }
+//            } else if (mass[0].equals("delete")) {
+//                try {
+//                    dataBase[Integer.parseInt(mass[1]) - 1] = "";
+//                    System.out.println("OK");
+//                } catch (IndexOutOfBoundsException e) {
+//                    System.out.println("ERROR");
+//                }
+//            } else {
+//                break;
+//            }
+//        }
+
 
     private static void initDataBase() {
         Arrays.fill(dataBase, "");
